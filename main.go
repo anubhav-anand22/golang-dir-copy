69577293f216668202f859e84a4a2d630fd0771e
Anubhav Anand <105984511+anubhav-anand22@users.noreply.github.com>
2024-03-27 17:54:08 +0000

    dropdown

--- main.go
+++ main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/mattn/go-tty"
+)
 
 func main() {
-   fmt.Println("kunal")
+	ttya, err := tty.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ttya.Close()
+
+	a := make([]string, 0)
+	a = append(a, "kunal")
+	a = append(a, "kumar")
+	a = append(a, "anu")
+	a = append(a, "bhav")
+	a = append(a, "anand")
+
+	dropdown(a, ttya)
+
+}
+
+func dropdownHelper(arr []string, selected int, title string) {
+	fmt.Println(title)
+	for index, element := range arr {
+		if index == selected {
+			fmt.Println(">>> " + element)
+		} else {
+			fmt.Println("    " + element)
+		}
+	}
+	fmt.Println("Press 's' to to go up, 'd' to go down and 'e' to select")
+}
+
+func dropdown(list []string, ttya *tty.TTY) int {
+
+	selected := 0
+	for {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+		dropdownHelper(list, selected, "ABC")
+		r, err := ttya.ReadRune()
+		if err != nil {
+			log.Fatal(err)
+		}
+		key := string(r)
+		if key == "e" {
+			break
+		} else if key == "s" {
+			if selected <= 0 {
+				selected = len(list) - 1
+			} else {
+				selected -= 1
+			}
+		} else if key == "d" {
+			if selected >= (len(list) - 1) {
+				selected = 0
+			} else {
+				selected += 1
+			}
+		}
+	}
+
+	return selected
 }
